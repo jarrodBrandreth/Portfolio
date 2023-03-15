4e79b0d95a08c42cf07f65cdea46db4ed6b9272c
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-03-15 18:07:37 +0100

    description

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -11,7 +11,7 @@ const plantShop:ProjectObj = {
   name:'The Plant Shop',
   image:plantshopImage,
   qrCode:qrPlantShop,
-  description:'Foo Coding project tasked with grasping the fundamentals of React. I built a mock plant e-commerce front end using React with TypeScript and JSON placeholder REST API to fetch data. Shopping cart, product views, sort by features and search filtering.',
+  description:'Foo Coding final project tasked with grasping the fundamentals of React. I built a e-commerce front end using React with TypeScript and JSON placeholder REST API to fetch data. Shopping cart, product views, sort by features and search filtering.',
   stack: 'React, TypeScript',
   hosted:'https://plant-shop.onrender.com/',
   source:'https://github.com/jarrodBrandreth/plant-shop-react',
@@ -21,7 +21,7 @@ const pokemonFun:ProjectObj = {
  name:'PokéFun',
   image:pokemonImage,
   qrCode:qrPokemonFun,
-  description:'A word and image guessing game built with React, TypeScript and using PokeApi REST API to fetch data. Ability to play games seperately or play tournament mode and collect points. Keyboard functionality as well as a graphical keyboard. Have fun!',
+  description:'Using the PokeApi RESTful api to fetch data, I built a word and image guessing game with React and TypeScript. Contains the ability to play games seperately or play tournament mode and collect points. Has keyboard functionality as well as a graphical keyboard. Have fun!',
   stack: 'React, TypeScript',
   hosted:'https://pokemon-game-test.onrender.com/',
   source:'https://github.com/jarrodBrandreth/react-pokemon-game',
@@ -31,10 +31,10 @@ const skatemaps:ProjectObj = {
  name:'NYC Skatemaps',
   image:skatemapsImage,
   qrCode:qrSkatemaps,
-  description:'A map of popular destinations for skateboarders in New York City. Contains a personalized map where you can bookmark, create, edit or delete your own locations as well as admin panel to perform CRUD operations app wide. Built using the MERN stack. *server is currently hosted for free so initial load may take some time.',
+  description:'A MERN full-stack app of popular destinations for skateboarders in New York City. Contains admin panel and a personalized map for users to bookmark, create, edit or delete your own locations. *server is currently hosted for free so initial load may take some time.',
   stack: 'React, TypeScript, NodeJS, Express, MongoDB',
   hosted:'https://skatemaps.onrender.com/',
   source:'https://github.com/jarrodBrandreth/SkateMapsClient',
 }
 
-export const projects:ProjectObj[] = [plantShop, pokemonFun, skatemaps];
+export const projects:ProjectObj[] = [skatemaps,plantShop, pokemonFun];
